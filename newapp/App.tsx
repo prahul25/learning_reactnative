61e0a126fce70f845159d3fbbf4397d49b6af6f9
RahulKumar Pali <rahul@sharedpro.in>
2026-05-30 23:46:22 +0530

    feat: add dice roll animation, styling, and sound effects

--- newapp/App.tsx
+++ newapp/App.tsx
@@ -1,13 +1,35 @@
-import { ScrollView, StyleSheet } from 'react-native'
-import React from 'react'
+import { ScrollView, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import PasswordGenerator from './component/PasswordGenerator'
 
 const App = () => {
+  const [randomBackgroundColor, setRandomBackgroundColor] = useState("#ffffff")
+
+  function generatedRandomBackgroundColor(){
+    const hexRange = "0123456789ABCDEF"
+    let color = "#"
+    
+    for (let i = 0; i < 6; i++) {
+      color += hexRange[Math.floor(Math.random() * 16)]
+    }
+    setRandomBackgroundColor(color)
+  }
   return (
-    <SafeAreaView style={styles.safe}>
+    <SafeAreaView style={[styles.safe,{backgroundColor:randomBackgroundColor}]} >
+      <StatusBar
+      animated={true}
+  backgroundColor={randomBackgroundColor}
+  barStyle="dark-content"
+/>
       <ScrollView contentContainerStyle={styles.scroll}>
         <PasswordGenerator />
+        <TouchableOpacity onPress={generatedRandomBackgroundColor}>
+          <View>
+            <Text>Click Me</Text>
+            {randomBackgroundColor && <Text>{randomBackgroundColor}</Text>}
+          </View>
+        </TouchableOpacity>
       </ScrollView>
     </SafeAreaView>
   )
@@ -23,5 +45,6 @@ const styles = StyleSheet.create({
   scroll: {
     flexGrow: 1,
     justifyContent: 'center',
+    alignItems:'center'
   },
 })
