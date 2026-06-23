b895538660c2431f9dcf1f17cb257a6cd551108a
RahulKumar Pali <rahul@sharedpro.in>
2026-06-23 23:00:13 +0530

    initial commit

---
+++ MusicApplication/App.tsx
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+
+function App() {
+
+  return (
+    <SafeAreaProvider>
+<View>
+        <Text>njnm kj</Text>
+</View>
+    </SafeAreaProvider>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
